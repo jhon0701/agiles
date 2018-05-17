60922426eb4d955d2f4c78ad92051d2631f1c662
jhon0701 <cjhon0701@gmail.com>
2018-05-17 16:30:12 -0400

    home

--- complaint/views.py
+++ complaint/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.urls import reverse
 # Form
 from .forms import ComplaintForm
+from .models import Complaint
 
 
 class ComplaintView(View):
@@ -37,10 +38,34 @@ class ComplaintPublicationView(View):
 
     def get(self, request):
         url = reverse("complaint_publication")
+        denuncias = Complaint.objects.all()
+        print (denuncias)
+        for d in denuncias:
+            d_name = d.complainant_name
+            d_place = d.place_event
+            d_complaint_date = d.complaint_date
+            d_date = d.event_date
+            d_time = d.event_time
+            d_description = d.description
+            d_license = d.license_plate
+            d_evidence = d.evidence
+            d_ranking = d.ranking
+
         context = {
             'url': url,
-            'title': self.TITLE
+            'title': self.TITLE,
+            "denuncias" :denuncias,
+            "d_name" :d_name,
+            "d_place" :d_place,
+            "d_complaint_date" :d_complaint_date,
+            "d_date" :d_date,
+            "d_time" :d_time,
+            "d_description" :d_description,
+            "d_license" :d_license,
+            "d_evidence" :d_evidence,
+            "d_ranking" :d_ranking
         }
+
         return render(request, self.template_name, context)
 
 
